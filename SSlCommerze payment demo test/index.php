f3a67e0785874f9ef3994b4b288cf78d422b4a72
MD Abidur Rahman <89156929+abid9837@users.noreply.github.com>
2024-01-28 15:45:58 +0600

    Add files via upload

---
+++ SSlCommerze payment demo test/index.php
@@ -0,0 +1,19 @@
+<?php
+$customer_name = "Abidbur Rahman";
+$amount = 1000;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="checkout.php?amount=<?php echo $amount;  ?>">Pay Now</a>
+
+</body>
+</html>
